6f5fdeeb8119e3ec031ae286351afb57bb87f107
Hüseyin Bilen <huseyinxbilen@gmail.com>
2022-07-04 11:06:19 +0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { HttpModule } from '@nestjs/axios';
     MoviesModule,
     UsersModule,
     MongooseModule.forRoot(
-      'mongodb://localhost:27017/nestjs-movie',
+      'mongodb://db:27017/nestjs-movie',
     ),
   ],
   controllers: [AppController],
